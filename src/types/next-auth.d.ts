9cc7f079bbe984cb62f3bc161ca90ede807c310d
sipp11 <sipp11@zzyzx.co>
2022-02-28 01:35:16 +0700

    Dataset Vote is working

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -6,8 +6,8 @@ declare module 'next-auth' {
    */
   interface Session {
     user: {
-      /** The user's postal address. */
-      address: string;
+      /** The user's ID. */
+      uid: string;
     } & DefaultSession['user'];
   }
 }
