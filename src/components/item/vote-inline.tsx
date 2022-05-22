b8d0ff6aca41ad35ae2430b077c90a5545953f4e
sipp11 <sipp11@zzyzx.co>
2022-05-22 17:49:56 +0700

    Like/Dislike separated

--- src/components/item/vote-inline.tsx
+++ src/components/item/vote-inline.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { gql, useMutation } from '@apollo/client';
 import dayjs from 'dayjs';
@@ -30,38 +30,18 @@ export default function VoteInline({
   const timer = useRef(0);
   const { status: sessStatus } = useSession();
   const [upsertVote] = useMutation(MUTATE_DATASET_POINTS);
-  const [Action, SetAction] = useState('-'); // 3 states: up, down, -
-  const [Point, SetPoint] = useState(+initialValue.up + +initialValue.down);
-  const [MyVote, SetMyVote] = useState(initialValue.mine);
+  const [MyPoint, SetMyPoint] = useState(+initialValue.mine);
+  const init = initialValue;
 
-  useEffect(() => {
-    if (initialValue.mine < 0) {
-      SetAction('down');
-    } else if (initialValue.mine === 0) {
-      SetAction('-');
-    } else {
-      SetAction('up');
-    }
-  }, [initialValue]);
-
-  function verifyIfrecorded(currScore, votingPoint, mutationResult) {
-    let latestPoint = currScore;
+  function verifyMutation(mutationResult) {
     try {
       const {
         insert_dataset_points: { returning },
       } = mutationResult.data;
       const confirmObj = returning[0];
-      latestPoint = confirmObj.dataset.points_aggregate.aggregate.sum.point;
-      const votedPoint = confirmObj.point;
-      if (votedPoint !== votingPoint) {
-        alert('Err: your vote does not count');
-        // setPoint(confirmObj.point)
-      }
+      SetMyPoint(confirmObj.point);
     } catch (e) {
       // console.log("verify[err] ", e)
-    } finally {
-      SetPoint(latestPoint);
-      SetHideNoAuth(true);
     }
   }
 
@@ -88,76 +68,62 @@ export default function VoteInline({
           datasetID,
         },
       });
-      verifyIfrecorded(Point, ownVal, result);
+      verifyMutation(result);
     }, 2000);
   }
 
-  function calcVote(action) {
-    let currValue = Point;
-    switch (action) {
-      case 'up':
-        currValue = currValue + 1 + (MyVote === -1 ? 1 : 0);
-        SetMyVote(1);
-        break;
-      case 'down':
-        currValue = currValue - 1 - (MyVote === 1 ? 1 : 0);
-        SetMyVote(-1);
-        break;
-      default:
-        if (MyVote > 0) {
-          currValue -= 1;
-        } else if (MyVote < 0) {
-          currValue += 1;
-        }
-        SetMyVote(0);
-    }
-    SetPoint(currValue);
-    SetAction(action);
-    saveVote(action);
-  }
-  let noColor = '';
-  if (Action === 'up') {
-    noColor = 'text-green-500';
-  } else if (Action === 'down') {
-    noColor = 'text-rose-500';
-  }
   /* Provider vote only applied to govOfficer #### >>> */
   return (
     <span className="inline">
       <div className={`text-md text-gray-600 flex items-center`}>
-        <span className={`font-mono text-xs ${noColor} pr-1`}>{Point}</span>
         <span
-          className="px-1 pb-1 hover:bg-slate-200"
-          title={wording.like}
-          onClick={() => {
-            if (sessStatus !== 'authenticated') {
-              SetHideNoAuth(false);
-              // alert(noActMsg);
-              return;
-            }
-            calcVote(Action === 'up' ? '-' : 'up');
-          }}
+          className={`flex items-center font-mono text-xs ${
+            MyPoint > 0 ? 'text-green-500' : 'text-gray-500'
+          }`}
         >
-          <ThumbUp
-            className="inline"
-            fill={MyVote > 0 ? `#10b981` : '#9CA3AF'}
-          />
+          {+init.up !== 0 && init.up}
+          <span
+            className="px-1 pb-1 hover:bg-slate-200"
+            title={wording.like}
+            onClick={() => {
+              if (sessStatus !== 'authenticated') {
+                SetHideNoAuth(false);
+                // alert(noActMsg);
+                return;
+              }
+              saveVote(MyPoint > 0 ? '-' : 'up');
+              // calcVote(Action === 'up' ? '-' : 'up');
+            }}
+          >
+            <ThumbUp
+              className="inline"
+              fill={MyPoint > 0 ? `#10b981` : '#9CA3AF'}
+            />
+          </span>
         </span>
         <span
-          className="px-1 pb-1 hover:bg-slate-200"
-          title={wording.unlike}
-          onClick={async () => {
-            if (sessStatus !== 'authenticated') {
-              SetHideNoAuth(false);
-              return;
-            }
-            calcVote(Action === 'down' ? '-' : 'down');
-          }}
+          className={`flex items-center font-mono text-xs ${
+            MyPoint < 0 ? 'text-rose-500' : 'text-gray-500'
+          }`}
         >
-          <ThumbDown
-            className="inline"
-            fill={MyVote < 0 ? `#fb7185` : '#9CA3AF'}
-          />
+          {+init.down !== 0 && Math.abs(+init.down)}
+          <span
+            className="px-1 pb-1 hover:bg-slate-200"
+            title={wording.unlike}
+            onClick={async () => {
+              if (sessStatus !== 'authenticated') {
+                SetHideNoAuth(false);
+                return;
+              }
+              saveVote(MyPoint < 0 ? '-' : 'down');
+              // calcVote(Action === 'down' ? '-' : 'down');
+            }}
+          >
+            <ThumbDown
+              className="inline"
+              fill={MyPoint < 0 ? `#fb7185` : '#9CA3AF'}
+            />
+          </span>
         </span>
       </div>
     </span>
